1a5f81042bb769435d5bd736240d6b3efb97d119
primovere <p.8izo3@slmail.me>
2026-04-30 11:52:05 +0800

    Add displayData to render weather results

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector(".display");
+
 async function getWeather(city) {
   const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=6699YUL929BVGXBW9L6FSM5NF&contentType=json`;
   console.log(URL);
@@ -6,7 +8,7 @@ async function getWeather(city) {
     const response = await fetch(URL);
     const weatherData = await response.json();
     const processed = processWeatherData(weatherData);
-    console.log(processed);
+    return processed;
   } catch (err) {
     console.error("City not found");
   }
@@ -18,4 +20,14 @@ function processWeatherData(data) {
     temp: data.currentConditions.temp,
     conditions: data.currentConditions.conditions,
   };
+}
+
+function displayData(data) {
+  display.innerHTML = `
+  <h2>${data.address}</h2>
+  <h3>Temperature</h3>
+  <span>${data.temp}</span>
+  <h3>Weather</h3>
+  <span>${data.conditions}</span>
+  `;
 }
